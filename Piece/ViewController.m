b30cbcb3b13850e42de59a566b6ceecdee4f4c88
youtaya <jxiaoping@gmail.com>
2015-11-15 20:28:35 +0800

    Fix segue issue

--- Piece/ViewController.m
+++ Piece/ViewController.m
@@ -17,8 +17,6 @@
 
 @implementation ViewController
 
-bool isLoginSucces = YES;
-
 - (IBAction)unwindToHome:(UIStoryboardSegue *)segue
 {
     
@@ -29,30 +27,27 @@ bool isLoginSucces = YES;
     NSString *username = self.usernameTextField.text;
     NSString *password = self.passwordTextField.text;
     
+    
+    
     [SimpleHttp requestLogin:username withPassword:password responseBlock:^(NSData *data, NSURLResponse *response, NSError *error) {
         if (!error) {
             NSDictionary *dict = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:nil];
             if (dict[@"error"]) {
                 NSLog(@"dictionary error : %@", dict[@"error"]);
-                isLoginSucces = NO;
             } else {
                 NSLog(@"%@", dict[@"username"]);
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self performSegueWithIdentifier:@"noteReview" sender:self];
+                });
+                
             }
         } else {
             NSLog(@"error : %@", error);
-            isLoginSucces = NO;
         }
     }];
     
 }
 
-- (BOOL) shouldPerformSegueWithIdentifier:(NSString *)identifier sender:(id)sender {
-    if ([identifier isEqualToString:@"noteReview"]) {
-        NSLog(@"login status : %d", isLoginSucces);
-        return isLoginSucces;
-    }
-    
-    return YES;
-}
 
 @end
